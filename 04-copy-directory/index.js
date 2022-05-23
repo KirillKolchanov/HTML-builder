0e80ffe528f10ccf1d8a69702f969271f2f2db1b
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-05-23 19:36:42 +0300

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 
+const createDir = path.join(__dirname, 'files-copy');
+const oldDir = path.join(__dirname, 'files');
+
+fs.readdir(oldDir, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.copyFile(oldDir + '/' + data[i], createDir + '/' + data[i], err => {if (err) throw err});
+  }
+})
+
+fs.promises.mkdir(createDir, {recursive: true});
