938aedc39b2b1e1313fe06bde50614024a448a0c
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-05-24 18:09:49 +0300

    fix: fix task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,76 +2,85 @@ const fs = require('fs');
 const path = require('path');
 
 const createDistDir = path.join(__dirname, 'project-dist');
-const createStylesDir = path.join(__dirname, 'project-dist', 'style.css');
-const copyDirAssets = path.join(__dirname, 'assets');
-const createAssetsDir = path.join(__dirname, 'project-dist', 'assets');
-const copyStyles = path.join(__dirname, 'styles');
+const createStylesFile = path.join(__dirname, 'project-dist', 'style.css');
+const assetsDir = path.join(__dirname, 'assets');
+const createDistPathAssets = path.join(__dirname, 'project-dist', 'assets');
+const copyStylesPath = path.join(__dirname, 'styles');
 const components = path.join(__dirname, 'components');
-const HTMLPath = fs.createReadStream('./06-build-page/template.html');
-
-let writeStyleStream = fs.createWriteStream(createStylesDir);
+const templateHTML = path.join(__dirname, 'template.html');
+const HTMLPath = fs.createReadStream(templateHTML, 'utf-8');
 
 fs.promises.mkdir(createDistDir, {recursive: true});
 
-fs.readdir(copyStyles, (err, files) => {
-  files.forEach(file => {
-    if (path.extname(file, err) == '.css') {
-      if (err) {
-        console.log(err);
-      } else {
-        const newStylesPath = path.join(__dirname, 'styles', file);
-        const stream = fs.createReadStream(newStylesPath);
-        stream.on('data', (data , err) => {
-          if (err) {
-            console.log(err);
-          }
-          writeStyleStream.write(data);
-        });
-      }
-    }
-  });
-});
+async function clearDir() {
+  try {
+    await fs.promises.rm(createDistDir, {recursive: true, force: true});
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-async function copyDir(copyDirAssets, createAssetsDir) {
-  await fs.promises.mkdir(createAssetsDir, {
+async function copeStyle() {
+  let writeStyleStream = fs.createWriteStream(createStylesFile);
+
+  fs.readdir(copyStylesPath, (err, data) => {
+    data.forEach(file => {
+      if (path.extname(file, err) == '.css') {
+        if (err) {
+          console.log(err);
+        } else {
+          const newStylesPath = path.join(__dirname, 'styles', file);
+          const stream = fs.createReadStream(newStylesPath);
+          stream.on('data', (chunk , err) => {
+            if (err) {
+              console.log(err);
+            }
+            writeStyleStream.write(chunk);
+          });
+        }
+      }
+    });
+  });
+}
+
+async function copyDir(assetsDir, createDistPathAssets) {
+  await fs.promises.mkdir(createDistPathAssets, {
     recursive: true
   });
 
-  const files = await fs.promises.readdir(copyDirAssets, {
+  const files = await fs.promises.readdir(assetsDir, {
     withFileTypes: true
   });
 
   files.forEach(async (file) => {
     if (file.isFile()) {
-      const oldFile = path.join(copyDirAssets, file.name);
-      const newFile = path.join(createAssetsDir, file.name);
+      const oldFile = path.join(assetsDir, file.name);
+      const newFile = path.join(createDistPathAssets, file.name);
       await fs.promises.copyFile(oldFile, newFile);
     } else {
-      copyDir(path.join(copyDirAssets, file.name), path.join(createAssetsDir, file.name));
+      copyDir(path.join(assetsDir, file.name), path.join(createDistPathAssets, file.name));
     }
   });
 }
 
-copyDir(copyDirAssets, createAssetsDir);
-
 async function createHtmlPage() {
-  const createHTML = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
+  const createHTMLBundel = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
   HTMLPath.on('data', (chunk) => {
     let templateString = chunk.toString();
 
     fs.readdir(components, {
       withFileTypes: true
-    }, (err, files) => {
+    }, (err, data) => {
       if (err) throw err;
-      files.forEach((file, i) => {
+      data.forEach((file, i) => {
         if (file.isFile() && path.parse(file.name).ext === '.html') {
           const readComponents = fs.createReadStream(path.join(__dirname, 'components', file.name));
           const nameComponents = path.parse(file.name).name;
-          const componentsObjectName = `{{${nameComponents}}}`;
+          const componentsObjecName = `{{${nameComponents}}}`;
           readComponents.on('data', (chunk) => {
-            templateString = templateString.replace(componentsObjectName, chunk.toString());
-            if (i === files.length - 1) {
-              createHTML.write(templateString);
+            templateString = templateString.replace(componentsObjecName, chunk.toString());
+            if (i === data.length - 1) {
+              createHTMLBundel.write(templateString);
             }
           });
         }
@@ -80,4 +89,9 @@ async function createHtmlPage() {
   });
 }
 
-createHtmlPage(console.log('Folder Project dist was created'));
+(async () => {
+  await clearDir();
+  await copyDir(assetsDir, createDistPathAssets);
+  await copeStyle();
+  await createHtmlPage();
+})();
