b02c554dfb53b611c6807d830053bb03eb4d1b5d
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-05-24 14:29:18 +0300

    fix: fix deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,13 @@ const path = require('path');
 const createDir = path.join(__dirname, 'files-copy');
 const oldDir = path.join(__dirname, 'files');
 
+fs.readdir(createDir, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.promises.unlink(createDir + '/' + data[i]);
+  }
+})
+
 fs.readdir(oldDir, (err, data) => {
   if (err) throw err;
   for (let i = 0; i < data.length; i++) {
